34c462dca7a4b5b21c6c84966ab46b8803798414
Rishav <suranarishav08@gmail.com>
2024-02-18 18:22:49 +0530

    update readme file

--- lib/util/error_msg.dart
+++ lib/util/error_msg.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather/constants/string_constants.dart' as string_constants;
 
-String? GetErrorMessage(BuildContext context, errorMsg) {
+String? getErrorMessage(BuildContext context, errorMsg) {
   return '${string_constants.pleaseEnter} $errorMsg';
 }
